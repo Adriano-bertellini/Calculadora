87dde837bb56ee332547f46b5c4677e19af0d469
Adriano-betellini <adrianoaugusto9090@gmail.com>
2024-02-04 00:43:40 -0300

    Divisão de botões

--- main.py
+++ main.py
@@ -3,18 +3,34 @@ from tkinter import ttk
 
 root = Tk()
 root.title("Calculadora")
-root.geometry('600x500')
-root.minsize(600, 500)
-root.maxsize(600, 500)
+root.geometry('408x305')
+root.minsize(408, 305)
+root.maxsize(408, 305)
 
 root.configure(background='#282828')
 
-e = Entry(root, width=15, borderwidth=4, relief=FLAT,fg='#FFFFFF', bg='#a7a28f', font =('futura', 25, 'bold'), justify=CENTER )
+e = Entry(root, width=15, borderwidth=4, relief=FLAT,fg='#282828', bg='#EEDFCC', font =('futura', 25, 'bold'), justify=CENTER )
 e.grid(
     row=0,
     column=0,
     columnspan=4,
     pady=2
 )
+
+def button_division():
+    return
+divide = Button(root,
+                text='÷',
+                bg= '#EEDFCC',
+                padx=40,
+                pady=20,
+                command=button_division(),
+                fg='#282828',
+                activebackground='#282828',
+                activeforeground='#240046',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+divide.grid(row=0, column=4)
 
 root.mainloop()
